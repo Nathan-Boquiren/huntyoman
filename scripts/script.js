12f5a154915931d23f768469297b39719aebfbc7
Nathan Boquiren <nathanjboquiren@gmail.com>
2025-01-24 16:35:04 -0500

    final hero

--- scripts/script.js
+++ scripts/script.js
@@ -135,7 +135,7 @@ window.addEventListener("scroll", function () {
 
   const parallaxElements = [
     { selector: ".featured-music", offset: 400 },
-    { selector: ".links", offset: 670 },
+    { selector: ".links", offset: 1000 },
     { selector: ".newsletter-section", offset: 1250 },
     { selector: "#release-countdown", offset: -200 },
     { selector: "#about-section", offset: 1000 },
